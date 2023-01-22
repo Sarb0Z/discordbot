04f2e116b90e4a749a365c85ea8b23e7f4dca4de
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-01-22 23:54:45 +0500

    tested if join and leave works

---
+++ main.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+const path=require('path')
+const fs = require ('fs');
+/*client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}*/
+
+
+client.once('ready', async() =>{
+    console.log('SarbzBot is ready');
+    client.user.setActivity('with the bruhs');
+    
+
+});
+
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'play') {
+        const { voice } = message.member
+        
+        if (!voice.channelID) {
+             message.reply('You must be in a voice channel')
+            return
+ 
+        }
+
+        voice.channel.leave()
+        message.channel.reply("Leaving channel")
+
+        
+        
+
+
+}
+})
+
+
+
+client.login('Nzc2MTc5MjgzNzQ3NTM2OTI3.X6xHSQ.cHhnV45GrtQIXA0vwNFW7TbxzYs')
